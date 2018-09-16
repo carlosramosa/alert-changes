c1063351440af5c4d1631220b9c80d112fb36e8b
carlosramosa <carlosramos.cramos@gmail.com>
2018-09-16 20:09:26 +0200

    feat(index): add loop to repeat the process each TTL

--- src/index.js
+++ src/index.js
@@ -14,25 +14,29 @@ const {
 
 const fs = require ('fs');
 
-capturaPantalla(URL)
-    .then( path => {
-        const b64 = base64_encode(path);
-        const haCambiado = comparar(b64);
-        //no son iguales -> notifico
-        if (haCambiado){
-            const imagenAnterior = {
-                base64: haCambiado
-            };
+const execute = async () => {
+    await capturaPantalla(URL)
+        .then( path => {
+            const b64 = base64_encode(path);
+            const haCambiado = comparar(b64);
+            //no son iguales -> notifico
+            if (haCambiado){
+                const imagenAnterior = {
+                    base64: haCambiado
+                };
 
-            fs.writeFileSync('anterior.json', JSON.stringify(imagenAnterior, null, 4));
+                fs.writeFileSync('anterior.json', JSON.stringify(imagenAnterior, null, 4));
 
-            console.log('No son iguales!! Notificando!!!');
-            notificar(haCambiado);
-        }
-        else {
-            console.log('Son iguales puto!!');
-        }
+                console.log('No son iguales!! Notificando!!!');
+                notificar(haCambiado);
+            }
+            else {
+                console.log('Son iguales puto!!');
+            }
 
-    });
+        });
+    setTimeout(pene, 15000);
+}
         
 
+execute();
